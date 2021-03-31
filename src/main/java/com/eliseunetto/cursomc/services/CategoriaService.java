28fb5f3049881957da5e659fce1a91a023595582
eliseunetto <eliseunetto@hotmail.com>
2021-03-31 15:30:08 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/eliseunetto/cursomc/services/CategoriaService.java
+++ src/main/java/com/eliseunetto/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.eliseunetto.cursomc.domain.Categoria;
 import com.eliseunetto.cursomc.repositories.CategoriaRepository;
+import com.eliseunetto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: "
+				+ id + ". Tipo: " + Categoria.class.getName()));
 	}
 }
