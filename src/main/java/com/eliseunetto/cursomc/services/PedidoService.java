8cc024c0a2263bb757873f6571e689680d948411
eliseunetto <eliseunetto@hotmail.com>
2021-04-11 03:23:44 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/eliseunetto/cursomc/services/PedidoService.java
+++ src/main/java/com/eliseunetto/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.eliseunetto.cursomc.domain.Cliente;
 import com.eliseunetto.cursomc.domain.ItemPedido;
 import com.eliseunetto.cursomc.domain.PagamentoComBoleto;
 import com.eliseunetto.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.eliseunetto.cursomc.domain.enums.EstadoPagamento;
 import com.eliseunetto.cursomc.repositories.ItemPedidoRepository;
 import com.eliseunetto.cursomc.repositories.PagamentoRepository;
 import com.eliseunetto.cursomc.repositories.PedidoRepository;
+import com.eliseunetto.cursomc.security.UserSS;
+import com.eliseunetto.cursomc.services.exceptions.AuthorizationException;
 import com.eliseunetto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -69,4 +75,13 @@ public class PedidoService {
 		return obj;
 	}
 
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.findById(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
+	}
 }
