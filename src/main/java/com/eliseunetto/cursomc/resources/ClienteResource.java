830b552f04f4fc422e17c98e9e8c5e9861c0f6c3
eliseunetto <eliseunetto@hotmail.com>
2021-04-19 18:51:50 -0300

    Enviando imagem via endpoint

--- src/main/java/com/eliseunetto/cursomc/resources/ClienteResource.java
+++ src/main/java/com/eliseunetto/cursomc/resources/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eliseunetto.cursomc.domain.Cliente;
@@ -84,5 +85,11 @@ public class ClienteResource {
 		Page<Cliente> list = clienteService.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value = "/picture", method = RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name = "file") MultipartFile file) {
+		URI uri = clienteService.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
 	}
 }
