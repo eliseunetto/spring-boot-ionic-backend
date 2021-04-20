ed41b2e560288e77ff5d10c387b23d025f8ca4e0
eliseunetto <eliseunetto@hotmail.com>
2021-04-19 21:33:40 -0300

    Ajustando o tamanho da imagem

--- src/main/java/com/eliseunetto/cursomc/services/ImageService.java
+++ src/main/java/com/eliseunetto/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -51,5 +52,17 @@ public class ImageService {
 		}
 	}
 	
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(
+				sourceImg, 
+				(sourceImg.getWidth()/2) - (min /2), 
+				(sourceImg.getHeight()/2) - (min/2), 
+				min,
+				min);
+	}
 	
+	public BufferedImage resize(BufferedImage sourceiImage, int size) {
+		return Scalr.resize(sourceiImage, Scalr.Method.ULTRA_QUALITY, size);
+	}
 }
